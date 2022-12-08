c2cee90d96f687002f2b8801cf7ed01f98f9ff70
LSokolovskaya <liubasokolovskaya@gmail.com>
2022-12-08 23:53:01 +0300

    Task4

--- Task10.1/Program.cs
+++ Task10.1/Program.cs
@@ -5,9 +5,9 @@
         static void Main(string[] args)
         {
             // Task1();
-            Task2();
+            // Task2();
             // Task3();
-            // Task4();
+            Task4();
             // Task5();
             // Task6();
             // Task7();
@@ -93,63 +93,64 @@
             {
                 System.Console.WriteLine("empty");
             }
-
-            static string MethodDelete(string simbol, ref string text)
-            {
-                text = "Тzыz zклаzсzсzнzыzйz zиz zуz zтzеzбzя zвсzеz zпоzлzуzчzиzтzсяz,z zхzорzоzшzеzгоz zдzнzя";
-                string result = "";
-                for (int i = 0; i < text.Length; i++)
-                {
-                    if (text[i].ToString() != simbol)
-                    {
-                        result += text[i];
-                    }
-                }
-                text = result;
-                return text;
-            }
-
-            #endregion
-            // #region Task4
-            // // 4. Написать метод выполняющий сложение парметов, если параметры одного типа данных возвращать true и результат сложение, в противном случае false (out)
-            // static void Task4()
-            // {
-            //     object number1 = 4;
-            //     object number2 = 6;
-            //     object text1 = "qwe";
-            //     object text2 = "fghj";
-            //     System.Console.WriteLine(MethodSum(number1, number2, text1, text2, out object result));
-            // }
-            // static object MethodSum(object number1, object number2, object text1, object text2, out object result)
-            // {
-
-            // }
-            // #endregion
-            #region Task5
-            // 5. Пользователь вводит строку и разделитель. Написать метод возвращающий наличие в строке разделителя, если разделитель есть вернуть строку разбитую данным разделителем (out)
-            static void Task5()
-            {
-                System.Console.WriteLine("Введите строку");
-                string text = Console.ReadLine();
-                System.Console.WriteLine("Введите сепаратор");
-                string separator = Console.ReadLine();
-            }
-            #endregion
-            #region Task6
-            // 6. Написать метод возвращающий разность между найбольшим и наименьшим значением из списка. Результат разности возращать через out, метод для поиска минимального и максимального написать с использованием params
-
-            #endregion
-            #region Task7
-            // 7. Написать метод который принимает строку, если строка содержит только 0 и 1 вернуть true, а 0 и 1 перевести в 10-ную систему счисления, в противном случае false (out)
-
-            #endregion
-            #region Task8
-            // 8. На входе метода набор 0 и 1 перевести в 16-ную систему счисления (params)
-
-            #endregion
         }
+        static string MethodDelete(string simbol, ref string text)
+        {
+            text = "Тzыz zклаzсzсzнzыzйz zиz zуz zтzеzбzя zвсzеz zпоzлzуzчzиzтzсяz,z zхzорzоzшzеzгоz zдzнzя";
+            string result = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i].ToString() != simbol)
+                {
+                    result += text[i];
+                }
+            }
+            text = result;
+            return text;
+        }
+
+        #endregion
+        #region Task4
+        // 4. Написать метод выполняющий сложение парметов, если параметры одного типа данных возвращать true и результат сложение, в противном случае false (out)
+        static void Task4()
+        {
+            string text1 = "qwe";
+            string text2 = "fghj";
+            System.Console.WriteLine(MethodSum(text1, text2, out string result));
+        }
+        static string MethodSum(string text1, string text2, out string result)
+        {
+            result = text1 is string && text2 is string ? text1 + text2 :
+                text1 is int && text2 is int ? text1 + text2 : "error";
+
+            return result;
+        }
+        #endregion
+        #region Task5
+        // 5. Пользователь вводит строку и разделитель. Написать метод возвращающий наличие в строке разделителя, если разделитель есть вернуть строку разбитую данным разделителем (out)
+        static void Task5()
+        {
+            System.Console.WriteLine("Введите строку");
+            string text = Console.ReadLine();
+            System.Console.WriteLine("Введите сепаратор");
+            string separator = Console.ReadLine();
+        }
+        #endregion
+        #region Task6
+        // 6. Написать метод возвращающий разность между найбольшим и наименьшим значением из списка. Результат разности возращать через out, метод для поиска минимального и максимального написать с использованием params
+
+        #endregion
+        #region Task7
+        // 7. Написать метод который принимает строку, если строка содержит только 0 и 1 вернуть true, а 0 и 1 перевести в 10-ную систему счисления, в противном случае false (out)
+
+        #endregion
+        #region Task8
+        // 8. На входе метода набор 0 и 1 перевести в 16-ную систему счисления (params)
+
+        #endregion
     }
 }
+
 
 
 
